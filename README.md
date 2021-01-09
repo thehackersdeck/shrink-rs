4eec09d46253dd264cfa4f67773956a65ee7a2b3
Stephen <appcypher@outlook.com>
2021-01-09 13:56:50 +0100

    Add basic code

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # shrink
 A Lean Data Exchange Format for the Web
+
+
+### FORMAT
+- Stack based
+
+##### Preamble
+The preamble is used to determine if data is little endian.
+
+- version - 1 byte - `1`
+- tag     - 1 byte - `s`
+
+
+##### Body
+
+**TYPES**
+- int8    - 1 byte - `0`
+- int16   - 1 byte - `1`
+- int32   - 1 byte - `2`
+- int64   - 1 byte - `3`
+- uint8   - 1 byte - `4`
+- uint16  - 1 byte - `5`
+- uint32  - 1 byte - `6`
+- uint64  - 1 byte - `7`
+- float32 - 1 byte - `8`
+- float64 - 1 byte - `9`
+- string  - 1 byte - `10`
+
+**MISC**
+- open  - 1 byte - `0`
+- close - 1 byte - `0`
+
+### TODOs
+- [ ] Conversion from shrink to JSON
+- [ ] Conversion from JSON to shrink
+- [ ] Deserializing from shrink to object
+- [ ] Serializing from object to shrink
+
---
+++ src/leb.rs
@@ -0,0 +1,246 @@
+pub type LEBResult<T> = Result<T, ParserError>;
+
+#[derive(Debug, Clone)]
+pub struct LEB<'a> {
+    pub(crate) data: &'a [u8], // The binary
+    pub(crate) cursor: usize,  // Used to track the current byte position as the parser advances.
+}
+
+impl<'a> LEB<'a> {
+    /// Creates new LEB converter.
+    pub fn new(data: &'a [u8]) -> Self {
+        LEB {
+            data,
+            cursor: 0, // cursor starts at first byte
+        }
+    }
+
+    /// Pushes an id into the parser's sections_consumed.
+    pub(super) fn push_section_id(&mut self, section_id: &u8) {
+        self.sections_consumed.push(*section_id);
+    }
+
+    pub fn data(&mut self) -> LEBResult<()> {
+        // Consume preamble.
+        self.data_preamble();
+        Ok(())
+    }
+
+    ///
+    pub fn data_preamble(&mut self) -> LEBResult<()> {
+        Ok(())
+    }
+
+
+    ///
+    pub fn skip_to_section(&mut self, section_id: u8) -> LEBResult<()> {
+        loop {
+            if section_id == self.section_id()? {
+                break;
+            }
+            let payload_len = self.peek_varuint32()?;
+            if !self.skip(payload_len as _) {
+                return Err(ParserError {
+                    kind: ErrorKind::BufferEndReached,
+                    cursor: self.cursor,
+                });
+            };
+        }
+        Ok(())
+    }
+
+    ///
+    pub fn peek_varuint32(&mut self) -> LEBResult<u32> {
+        let cursor = self.cursor;
+        let value = self.varuint32();
+        self.cursor = cursor;
+        match value {
+            Ok(value) => Ok(value),
+            Err(kind) => Err(ParserError { kind, cursor }),
+        }
+    }
+
+    ///
+    pub fn skip(&mut self, len: usize) -> bool {
+        let jump = self.cursor + len + 1;
+        // Check if jump is within data buffer bounds
+        if jump > self.data.len() {
+            return false;
+        }
+        true
+    }
+
+    /// Gets a byte from the data buffer and (if available)
+    /// advances the cursor.
+    pub fn eat_byte(&mut self) -> Option<u8> {
+        let index = self.cursor;
+        // Check if range is within data buffer bounds
+        if index < self.data.len() {
+            // Advance the cursor
+            self.cursor += 1;
+            return Some(self.data[index]);
+        }
+        None
+    }
+
+    /// Gets the next `range` slice of bytes from the data buffer
+    /// (if available) and advances the token.
+    pub fn eat_bytes(&mut self, range: usize) -> Option<&[u8]> {
+        let start = self.cursor;
+        let end = start + range;
+        // Check if range is within data buffer bounds
+        if end > self.data.len() {
+            return None;
+        }
+        // Advance the cursor
+        self.cursor = end;
+        Some(&self.data[start..end])
+    }
+
+    /// Consumes 1 byte that represents an 8-bit unsigned integer
+    pub fn uint8(&mut self) -> Result<u8, ErrorKind> {
+        if let Some(byte) = self.eat_byte() {
+            return Ok(byte);
+        }
+        Err(ErrorKind::BufferEndReached)
+    }
+
+    /// Consumes a byte that represents a 1-bit LEB128 unsigned integer encoding
+    pub fn varuint1(&mut self) -> Result<bool, ErrorKind> {
+        if let Some(byte) = self.eat_byte() {
+            return match byte {
+                1 => Ok(true),
+                0 => Ok(false),
+                _ => Err(ErrorKind::MalformedVaruint1),
+            };
+        }
+        // We expect the if statement to return an Ok result. If it doesn't
+        // then we are trying to read more than 1 byte, which is malformed for a varuint1
+        Err(ErrorKind::BufferEndReached)
+    }
+
+    /// Consumes a byte that represents a 7-bit LEB128 unsigned integer encoding
+    pub fn varuint7(&mut self) -> Result<u8, ErrorKind> {
+        if let Some(byte) = self.eat_byte() {
+            let result = byte;
+            // Check if msb is unset.
+            if result & 0b1000_0000 != 0 {
+                return Err(ErrorKind::MalformedVaruint7);
+            }
+            return Ok(result);
+        }
+        // We expect the if statement to return an Ok result. If it doesn't
+        // then we are trying to read more than 1 byte, which is malformed for a varuint7
+        Err(ErrorKind::BufferEndReached)
+    }
+
+    /// Consumes 1-5 bytes that represent a 32-bit LEB128 unsigned integer encoding
+    pub fn varuint32(&mut self) -> Result<u32, ErrorKind> {
+        // verbose!("-> varuint32! <-");
+        let mut result = 0;
+        let mut shift = 0;
+        while shift < 35 {
+            let byte = match self.eat_byte() {
+                Some(value) => value,
+                None => return Err(ErrorKind::BufferEndReached),
+            };
+            // verbose!("(count = {}, byte = 0b{:08b})", count, byte);
+            // Unset the msb and shift by multiples of 7 to the left
+            let value = ((byte & !0b1000_0000) as u32) << shift;
+            result |= value;
+            // Return if any of the bytes has an unset msb
+            if byte & 0b1000_0000 == 0 {
+                return Ok(result);
+            }
+            shift += 7;
+        }
+        // We expect the loop to terminate early and return an Ok result. If it doesn't
+        // then we are trying to read more than 5 bytes, which is malformed for a varuint32
+        Err(ErrorKind::MalformedVaruint32)
+    }
+
+    /// Consumes a byte that represents a 7-bit LEB128 signed integer encoding
+    pub fn varint7(&mut self) -> Result<i8, ErrorKind> {
+        if let Some(byte) = self.eat_byte() {
+            let mut result = byte;
+            // Check if msb is unset.
+            if result & 0b1000_0000 != 0 {
+                return Err(ErrorKind::MalformedVarint7);
+            }
+            // If the 7-bit value is signed, extend the sign.
+            if result & 0b0100_0000 == 0b0100_0000 {
+                result |= 0b1000_0000;
+            }
+            return Ok(result as i8);
+        }
+
+        Err(ErrorKind::BufferEndReached)
+    }
+
+    /// Consumes 1-5 bytes that represent a 32-bit LEB128 signed integer encoding
+    pub fn varint32(&mut self) -> Result<i32, ErrorKind> {
+        // verbose!("-> varint32! <-");
+        let mut result = 0;
+        let mut shift = 0;
+        // Can consume at most 5 bytes
+        while shift < 35 {
+            // (shift = 0, 7, 14 .. 35)
+            let byte = match self.eat_byte() {
+                Some(value) => value,
+                None => return Err(ErrorKind::BufferEndReached),
+            };
+            // verbose!("(count = {}, byte = 0b{:08b})", count, byte);
+            // Unset the msb and shift by multiples of 7 to the left
+            let value = ((byte & !0b1000_0000) as i32) << shift;
+            result |= value;
+            // Return if any of the bytes has an unset msb
+            if byte & 0b1000_0000 == 0 {
+                // Extend sign if sign bit is set. We don't bother when we are on the 5th byte
+                // (hence shift < 28) because it gives an 32-bit value, so no need for sign
+                // extension there
+                if shift < 28 && byte & 0b0100_0000 != 0 {
+                    result |= -1 << (7 + shift); // -1 == 0xff_ff_ff_ff
+                }
+                return Ok(result);
+            }
+            shift += 7;
+        }
+        // We expect the loop to terminate early and return an Ok result. If it doesn't
+        // then we are trying to read more than 5 bytes, which is malformed for a varint32
+        Err(ErrorKind::MalformedVarint32)
+    }
+
+    ///
+    /// Consumes 1-9 bytes that represent a 64-bit LEB128 signed integer encoding
+    pub fn varint64(&mut self) -> Result<i64, ErrorKind> {
+        // verbose!("-> varint64! <-");
+        let mut result = 0;
+        let mut shift = 0;
+        // Can consume at most 9 bytes
+        while shift < 63 {
+            // (shift = 0, 7, 14 .. 56)
+            let byte = match self.eat_byte() {
+                Some(value) => value,
+                None => return Err(ErrorKind::BufferEndReached),
+            };
+            // verbose!("(count = {}, byte = 0b{:08b})", count, byte);
+            // Unset the msb and shift by multiples of 7 to the left
+            let value = ((byte & !0b1000_0000) as i64) << shift;
+            result |= value;
+            // Return if any of the bytes has an unset msb
+            if byte & 0b1000_0000 == 0 {
+                // Extend sign if sign bit is set. We don't bother when we are on the 9th byte
+                // (hence shift < 56) because it gives an 64-bit value, so no need for sign
+                // extension there
+                if shift < 56 && byte & 0b0100_0000 != 0 {
+                    result |= -1 << (7 + shift); // -1 == 0xff_ff_ff_ff
+                }
+                return Ok(result);
+            }
+            shift += 7;
+        }
+        // We expect the loop to terminate early and return an Ok result. If it doesn't
+        // then we are trying to read more than 5 bytes, which is malformed for a varint64
+        Err(ErrorKind::MalformedVarint64)
+    }
+}
